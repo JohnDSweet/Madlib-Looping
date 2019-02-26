4132e2d57b3c8c8ff379230cdcb3ff13277e50d9
Student <example@example.com>
2019-02-25 20:31:12 -0800

    Add looping practice.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var placeInput = $("input#place").val();
-    var verbInput= $("input#verb").val();
-    var adjectiveInput = $("input#adjective").val();
 
+    var blanks = ["person1", "place", "verb", "adjective"];
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
-    $(".person1").text(person1Input);
-    $(".place").text(placeInput);
-    $(".verb").text(verbInput);
-    $(".adjective").text(adjectiveInput);
-
+    var elements = ["h1", "p", "label", "input", "button"];
+    elements.forEach(function(element) {
+      $(element).click(function() {
+        alert("This is a " + element + ".");
+      });
+    });
 
     $("#story").show();
 
